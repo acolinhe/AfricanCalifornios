1daea04c63faa9c2eb902a562609d0e0c0369ba1
Anthony <acolinhe@gmail.com>
2024-05-15 01:14:24 -0700

    Implemented overall Family Tree. Need to go up/down generations

--- source_family_trees/family_tree_script.py
+++ source_family_trees/family_tree_script.py
@@ -1,14 +1,19 @@
 import logging
 from typing import Optional
 import pandas as pd
+import os
+import json
 from family_unit import FamilyUnit, PersonUnit
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def read_people_collect() -> Optional[pd.DataFrame]:
+def read_data(file_name: str) -> Optional[pd.DataFrame]:
     """ Load data from a CSV file and handle potential errors. """
-    people_collect_path = '/datasets/acolinhe/data_output/people_collect_2.csv'
+    current_directory = os.getcwd()
+    matches_directory = os.path.join(current_directory, '..', 'people_collect')
+    people_collect_path = os.path.join(matches_directory, file_name)
+
     try:
         return pd.read_csv(people_collect_path)
     except FileNotFoundError:
@@ -19,11 +24,62 @@ def read_people_collect() -> Optional[pd.DataFrame]:
         return None
 
 
+def add_identifiers(df, id_column_name):
+    """ Add sequential identifiers to a dataframe. """
+    df[id_column_name] = range(1, len(df) + 1)
+
+
+# if person has no children, then empty list
+def grab_children(row_id, df: pd.DataFrame) -> list:
+    row = df.loc[df['ecpp_id'] == row_id]
+
+    child_columns = [col for col in df.columns if col.startswith('Child')]
+    children_list = row[child_columns].values.flatten().tolist()
+    children_list = [child for child in children_list if pd.notna(child)]
+
+    return children_list
+
+
+def serialize_family_units_to_json(family_units, filepath):
+    family_units_dict = {
+        "family_units": [family_unit.to_dict() for family_unit in family_units]
+    }
+
+    with open(filepath, 'w') as json_file:
+        json.dump(family_units_dict, json_file, indent=4)
+
+
+# working with pc2, baptisms, and 1790 census for now
+# need to add spouse and go down and up family trees (ask dr. Jones!)
 def main():
+    pid = 101
     # put in main for now and abstract away with functions
-    pc2 = read_people_collect()
-    print(pc2)
-    family = FamilyUnit(0)
+    census_1790 = read_data('1790 Census Data Complete.csv')
+    baptisms = read_data('Baptisms.csv')
+    add_identifiers(census_1790, 'ecpp_id')
+    add_identifiers(baptisms, '#ID')
+    pc2 = read_data('people_collect_2.csv')
+    family_units = []
+
+    for index, row in pc2.iterrows():
+        family = FamilyUnit(index)
+        person = PersonUnit(pid,
+                            name=row['first_name'],
+                            sex=row['sex'],
+                            race=row['race_aggregated'],
+                            ethnicity=row['ethnicity'],
+                            baptismal_date=[row['baptismal_date']], )
+        pid += 1
+
+        for ch in grab_children(row['ecpp_id'], census_1790):
+            child = PersonUnit(pid, name=ch)
+            pid += 1
+            person.add_potential_child(child)
+
+        family.add_member(person)
+        family_units.append(family)
+
+    serialize_family_units_to_json(family_units, 'family_units.json')
 
 
 if __name__ == '__main__':
--- source_family_trees/family_unit.py
+++ source_family_trees/family_unit.py
@@ -4,6 +4,9 @@ class FamilyUnit:
         self.fid = fid
         self.members = []
 
+    def __str__(self):
+        return str(self.to_dict())
+
     def add_member(self, person_unit):
         self.members.append(person_unit)
         person_unit.family_unit = self
@@ -20,15 +23,15 @@ class FamilyUnit:
 
 # Goal is to store all information from PC2
 class PersonUnit:
-    def __init__(self, pid, name, children=None, spouse=None, sex=None, race=None, ethnicity=None, baptismal_date=None):
+    def __init__(self, pid, name, sex=None, race=None, ethnicity=None, baptismal_date=None, children=None, spouse=None):
         self.pid = pid
         self.name = name
-        self.potential_children = children if children else []
-        self.potential_spouses = spouse if spouse else []
         self.sex = sex
         self.race = race
         self.ethnicity = ethnicity
         self.baptismal_date = baptismal_date
+        self.potential_children = children if children else []
+        self.potential_spouses = spouse if spouse else []
 
     def add_potential_child(self, child):
         if child not in self.potential_children:
@@ -42,10 +45,13 @@ class PersonUnit:
         return {
             "pid": self.pid,
             "name": self.name,
-            "potential_children": [child.to_dict() for child in self.potential_children],
-            "potential_spouses": [spouse.to_dict() for spouse in self.potential_spouses],
             "sex": self.sex,
             "race": self.race,
             "ethnicity": self.ethnicity,
-            "baptismal_date": self.baptismal_date
+            "baptismal_date": self.baptismal_date,
+            "potential_children": [child.to_dict() for child in self.potential_children],
+            "potential_spouses": [spouse.to_dict() for spouse in self.potential_spouses],
         }
+
+    def __str__(self):
+        return str(self.to_dict())
